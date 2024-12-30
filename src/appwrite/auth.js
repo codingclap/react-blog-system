bf25148b4d573e4ea4c0ba9e0d5217fe61a28b46
Anuj Kumar <86232374+codingclap@users.noreply.github.com>
2024-12-30 18:33:21 +0530

    auth done

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,3 +1,44 @@
-export class AuthService{
-     
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client.setEndpoint(conf.appwriteURL).setProject(conf.appwriteProjectID);
+        this.account = new Account(this.client);
+    }
+
+
+    async createAccount({ name, email, password }) {
+        try {
+            const userAccount = await this.account.create(ID.unique, email, password, name);
+            if (userAccount) {
+                // Call Authorized method
+                this.login({ email, password })
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        
+    }
+
 }
+
+
+const authService = new AuthService();
+export default authService;
